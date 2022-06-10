5b90f014b852560e2818e777f43331996c77c020
alisson <alissonfsb14@hotmail.com>
2022-06-09 23:30:14 -0300

    verificando escopo de variáveis e teste com expressões numéricas

--- sintaxe-variaveis-e-fluxo/src/main/java/TesteEscopo.java
+++ sintaxe-variaveis-e-fluxo/src/main/java/TesteEscopo.java
@@ -1,9 +1,18 @@
-public class TestaCondicional2 {
+public class TesteEscopo {
     public static void main(String[] args) {
         System.out.println("testando Condicionais");
         int idade = 18;
         int quantidadeDePessoas = 3;
-        boolean acompanhado = quantidadeDePessoas >= 2;
+        //boolean acompanhado = quantidadeDePessoas >= 2;
+
+        boolean acompanhado;
+        if (quantidadeDePessoas >= 2){
+            acompanhado = true;
+        }
+        else {
+            acompanhado = false;
+        }
+
         if (idade >= 18 && acompanhado) {
             System.out.println("Você tem mais de 18 anos");
         } else {
@@ -11,4 +20,3 @@ public class TestaCondicional2 {
         }
     }
 }
-
