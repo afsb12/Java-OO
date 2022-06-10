cac87236c5fc81990461ef8fadd799f70a044bf0
alisson <alissonfsb14@hotmail.com>
2022-06-09 23:40:39 -0300

    criando teste para Imposto de renda

--- sintaxe-variaveis-e-fluxo/src/main/java/TesteIR.java
+++ sintaxe-variaveis-e-fluxo/src/main/java/TesteIR.java
@@ -1,2 +1,24 @@
 public class TesteIR {
+    public static void main(String[] args) {
+
+        double salario = 300.0;
+
+        if (salario < 1900.0) {
+            System.out.println("funcionário isento do imposto de renda");
+        } else if (salario >= 1900.0 && salario <= 2800.0) {
+            salario = salario - 142;
+            System.out.println("Porcentagem do IR para a faixa salarial do funcionário é de 7.5%.");
+            System.out.println("seu salário final será: " + salario);
+        } else if (salario >= 2800.0 && salario <= 3751.0) {
+            salario = salario - 142;
+            System.out.println("Porcentagem do IR para a faixa salarial do funcionário é de 15%.");
+            System.out.println("seu salário final será: " + salario);
+        } else if (salario >= 3751.01 && salario <=4664.0) {
+            salario = salario - 142;
+            System.out.println("Porcentagem do IR para a faixa salarial do funcionário é de 22%.");
+            System.out.println("seu salário final será: " + salario);
+        }else{
+            System.out.println("Parabéns você é rico.");
+        }
+    }
 }
