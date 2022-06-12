d04d888265a5e368fb7b0c045dba65d356200578
alisson <alissonfsb14@hotmail.com>
2022-06-12 20:18:41 -0300

    instanciando objetos

--- bytebanck/src/main/java/CriaConta.java
+++ bytebanck/src/main/java/CriaConta.java
@@ -3,5 +3,14 @@ public class CriaConta {
        Conta primeiraConta =  new Conta();
        primeiraConta.saldo = 2000.0;
        System.out.println(primeiraConta.saldo);
+       primeiraConta.saldo += 100;
+       System.out.println(primeiraConta.saldo);
+
+       Conta segundaConta = new Conta();
+       segundaConta.saldo = 5000.0;
+
+       System.out.println("Primeira conta tem: " + primeiraConta.saldo);
+       System.out.println("Segunda conta tem: " + segundaConta.saldo);
+
     }
 }
